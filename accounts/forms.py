9a92c7726ec976d718f53f3f7d2ccf1495355426
0xZaki <ahmed.zaki1998@outlook.com>
2025-02-21 20:59:47 +0200

    feat : add initial authentications

---
+++ accounts/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
+
+
+class CustomUserCreationForm(UserCreationForm):
+    """
+    A form for creating new users.
+    extends UserCreationForm to change the model
+    """
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
---
+++ accounts/views.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+
+from .forms import CustomUserCreationForm
+
+
+class CustomLoginView(LoginView):
+    """
+    Custom login view that extends the default login view to
+    - redirect to the home page if the user is already authenticated
+    - change the template
+    """
+    template_name = 'accounts/login.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('core:home')
+        return super().dispatch(request, *args, **kwargs)
+
+
+class RegisterView(TemplateView):
+    template_name = 'accounts/register.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('core:home')
+        form = CustomUserCreationForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:login')
+        return render(request, self.template_name, {'form': form})
