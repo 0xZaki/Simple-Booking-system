44838f54210e910db64dc5321b6a01bbb9bd191b
0xZaki <ahmed.zaki1998@outlook.com>
2025-02-24 00:36:05 +0200

    fix : fix celery running entrypoint & fix ports

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,6 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
+python manage.py loaddata fixtures/*
+
 exec gunicorn --bind 0.0.0.0:8000 --workers 3 --timeout 120 config.wsgi:application
